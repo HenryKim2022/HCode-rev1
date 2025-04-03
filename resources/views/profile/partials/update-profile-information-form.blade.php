fdc2c89dfd3031d7168109eadac0eb951fc845ce
HenryKim2022 <autromatron@gmail.com>
2025-04-03 23:38:55 +0700

    `Refactor system settings page and maintenance mode functionality`

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -28,6 +28,9 @@
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
+
+                {{  getAccountByEmail($user->email)['name'] }}
+
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800">
